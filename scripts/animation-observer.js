09e661192212386f5c811b8a02489170ec5e6ded
Vít Dobrovský <dobrovsky.vit@gmail.com>
2025-04-12 14:28:58 +0200

    animation fixes

--- scripts/animation-observer.js
+++ scripts/animation-observer.js
@@ -1,11 +1,16 @@
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("animate");
-      observer.unobserve(entry.target);
-    }
-  });
-});
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("animate");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 1,
+  }
+);
 
 document.querySelectorAll(".rectangle").forEach((el) => {
   observer.observe(el);
