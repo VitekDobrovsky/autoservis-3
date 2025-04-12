17f0271dcf434ae0f79a248175a6c2043e9807d0
Vít Dobrovský <dobrovsky.vit@gmail.com>
2025-04-12 12:23:36 +0200

    animation trigger

---
+++ scripts/animation-observer.js
@@ -0,0 +1,12 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("animate");
+      observer.unobserve(entry.target);
+    }
+  });
+});
+
+document.querySelectorAll(".rectangle").forEach((el) => {
+  observer.observe(el);
+});
