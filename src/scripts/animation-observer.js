9363dbf725bae550334aa4663332ff050cfcced3
Vít Dobrovský <dobrovsky.vit@gmail.com>
2025-04-18 11:00:08 +0200

    better navbar animations

--- src/scripts/animation-observer.js
+++ src/scripts/animation-observer.js
@@ -18,9 +18,15 @@ document.querySelectorAll(".rectangle").forEach((el) => {
 
 window.addEventListener("scroll", () => {
   const header = document.querySelector(".page__header");
+  const navbar = document.querySelector(".page__navbar");
   if (window.scrollY > 24) {
     header.classList.add("active");
   } else {
     header.classList.remove("active");
+  }
+  if (window.scrollY > 650) {
+    navbar.classList.add("active");
+  } else {
+    navbar.classList.remove("active");
   }
 });
