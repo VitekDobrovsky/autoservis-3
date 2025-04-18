f84d534641d81ef3bc5463f2981f6e01aba59c32
Vít Dobrovský <dobrovsky.vit@gmail.com>
2025-04-18 09:18:10 +0200

    navbar scroll animation

--- src/scripts/animation-observer.js
+++ src/scripts/animation-observer.js
@@ -15,3 +15,12 @@ const observer = new IntersectionObserver(
 document.querySelectorAll(".rectangle").forEach((el) => {
   observer.observe(el);
 });
+
+window.addEventListener("scroll", () => {
+  const header = document.querySelector(".page__header");
+  if (window.scrollY > 24) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
